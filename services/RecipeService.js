4a3e61712f7af170928455da8ba6d88d9ea2afce
jaypatel <jjpatel361@gmail.com>
2016-11-06 00:56:06 -0400

    Normalizing alexa output

--- services/RecipeService.js
+++ services/RecipeService.js
@@ -1,6 +1,8 @@
 var recipestore = require('../datastore/recipe.json');
 
 module.exports.getProcedure = function (recipe) {
+  // Do normalize because alexa sends Sentence case
+  var recipe = recipe.toUpperCase().replace(/ /g,"_");
   if(recipe in recipestore){
       return recipestore[recipe]
   }else {
